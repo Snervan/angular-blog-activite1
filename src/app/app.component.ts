d0aaea7ef646c2bd1bc2dd15d98c811c3531466e
Snervan <defevera@gmail.com>
2018-05-31 18:05:47 +0200

    Adding "Posts" in AppComponent
    Adding "POSTS" in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,22 @@ export class AppComponent {
   Posts = [
   {
     title: 'Mon premier post',
-    content:'Lorem ipsum dolor sit amet',
+    content:'Donec quis vestibulum nisi, eu tincidunt odio. Phasellus sed urna sem. Pellentesque in lacus bibendum, finibus est vel, euismod massa.',
     loveIts: 0,
     created_at: Date.now()
-  }];
+  },
+  {
+    title: 'Mon second post',
+    content:'Donec quis vestibulum nisi, eu tincidunt odio. Phasellus sed urna sem. Pellentesque in lacus bibendum, finibus est vel, euismod massa.',
+    loveIts: 2,
+    created_at: Date.now()
+  },
+  {
+    title: 'Mon troisième post',
+    content:'Aenean viverra, quam nec imperdiet hendrerit, metus magna cursus nisi, non suscipit nisi enim id orci. ',
+    loveIts: -6,
+    created_at: Date.now()
+  },];
 
 
   constructor(){
